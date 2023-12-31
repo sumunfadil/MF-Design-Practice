954d958e5a8ae5ee9eced6ec4c749de6faccf682
Fadil Sumun <sumun.fadil@gmail.com>
2023-12-31 22:11:12 +0000

    Chapter 4 started

---
+++ 2006 - Duffy - Introduction to C++/Chapter 4/Problem 1/CallByValueFunction.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+double Max(double x, double y)
+{
+    if (x > y) return x;
+    
+    return y;
+}
+
+int main()
+{
+    // Press run/ debug button for small C++ scripts
+    double d1 = 1.0;
+    double d2 = -345.0;
+
+    double result = Max(d1, d2);
+    cout << "Max value is: " << result << endl;
+
+    return 0;
+}
